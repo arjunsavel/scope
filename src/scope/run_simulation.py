6a65bd24417101615172bb77cbccde6c69a3ebbb
arjunsavel <asavel@gmail.com>
2024-08-16 14:08:29 -0400

    convolve correctly

--- src/scope/run_simulation.py
+++ src/scope/run_simulation.py
@@ -415,7 +415,8 @@ def simulate_observation(
     wl_model = wl_model.astype(np.float64)
 
     # Fp_conv_rot = broaden_spectrum(wl_model / 1e6, Fp, 0, vl=v_rot)
-    Fp_conv_rot = get_rot_ker(wl_model, Fp, v_rot)
+    rot_ker = get_rot_ker(v_rot, wl_model)
+    Fp_conv_rot = np.convolve(Fp, rot_ker, mode="same")
 
     # instrument profile convolution
     xker = np.arange(41) - 20
