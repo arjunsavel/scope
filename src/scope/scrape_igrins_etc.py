939e88fc6dbbe2c4b5660bde009f0dcb1b40061d
arjunsavel <asavel@gmail.com>
2025-03-05 00:12:28 -0500

    use chrome options

--- src/scope/scrape_igrins_etc.py
+++ src/scope/scrape_igrins_etc.py
@@ -17,7 +17,6 @@ from selenium.common.exceptions import ElementNotInteractableException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
